4e6a279c1047003bcd24e74d9e363f2df80497cf
ant396 <123@123.com>
2017-08-01 19:07:49 +0300

    testing A

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+
 int main(void)
 {
 	printf("hello world\n");
+	printf("i try to learn branches\n");
 	return 0;
 }
---
+++ test_a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("new\n");
+	return 0;
+}
